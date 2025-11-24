587981dfdc8fde5c5d6364969a8024936223ca19
tarikBelhaj <108001615+tarikBelhaj@users.noreply.github.com>
2025-11-24 16:45:32 +0100

    Change image URL for helicopter tour experience
    Updated the image URL for the helicopter tour experience.

--- services/experiencesApi.ts
+++ services/experiencesApi.ts
@@ -11,7 +11,7 @@ const experiences: Experience[] = [
   {
     title: 'Tour en hélicoptère des Alpes',
     description: 'Survolez les sommets enneigés et les glaciers.',
-    image: 'https://images.unsplash.com/photo-1502514262256-42d50b9a9f5c?q=80&w=800&auto=format&fit=crop',
+    image: 'https://images.unsplash.com/photo-1726212859367-ffb30266292f?q=80&w=1170&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
     badge: 'Exclusivité',
     bookingUrl: 'https://www.getyourguide.com/geneva-l28/panoramic-helicopter-tour-over-the-swiss-alps-from-geneva-t61248/'
   },
