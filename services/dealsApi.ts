c6893c5098438aea935ca81de44fa424cf4adbbf
tarikBelhaj <108001615+tarikBelhaj@users.noreply.github.com>
2025-11-24 16:36:16 +0100

    Update image URL for Gstaad Palace deal

--- services/dealsApi.ts
+++ services/dealsApi.ts
@@ -12,7 +12,7 @@ const mockDeals: Deal[] = [
         id: 'deal-gstaad',
         title: 'Gstaad Palace', 
         subtitle: '-25% Suite',
-        image: 'https://unsplash.com/fr/photos/une-vue-dune-montagne-enneigee-avec-des-arbres-dessus-a89K4YbINP8',
+        image: 'https://images.unsplash.com/photo-1642929775672-c24d399deda1?q=80&w=627&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
         page: 'hotels'
     },
     { 
