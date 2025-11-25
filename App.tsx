3e179f21281177e0e8e3830a81cc48212ba503ef
tarikBelhaj <108001615+tarikBelhaj@users.noreply.github.com>
2025-11-25 10:56:12 +0100

    feat: Update category images
    Replaces outdated placeholder images for hotels, cars, transfers, and experiences with new, higher-quality visuals. This enhances the application's aesthetic appeal and user experience.

--- App.tsx
+++ App.tsx
@@ -41,28 +41,28 @@ const InnerApp: React.FC = () => {
     {
       title: t('hotelsTitle'),
       subtitle: t('hotelsSubtitle'),
-      image: 'https://images.unsplash.com/photo-1566073771259-6a8506099945?q=80&w=800&auto=format&fit=crop',
+      image: 'https://images.unsplash.com/photo-1643654282922-a859073eb6a1?q=80&w=1170&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
       badge1: t('hotelsBadge'),
       page: 'hotels'
     },
     {
       title: t('carsTitle'),
       subtitle: t('carsSubtitle'),
-      image: 'https://images.unsplash.com/photo-1552519507-da3b142c6e3d?q=80&w=800&auto=format&fit=crop',
+      image: 'https://images.unsplash.com/photo-1592198084033-aade902d1aae?q=80&w=1170&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
       badge1: t('carsBadge'),
       page: 'cars'
     },
     {
       title: t('transfersTitle'),
       subtitle: t('transfersSubtitle'),
-      image: 'https://images.unsplash.com/photo-1549317661-bd32c8ce0db2?q=80&w=800&auto=format&fit=crop',
+      image: 'https://images.unsplash.com/photo-1627835448014-d4acd4d7cf49?q=80&w=1170&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
       badge1: t('transfersBadge'),
       page: 'transfers'
     },
     {
       title: t('experiencesTitle'),
       subtitle: t('experiencesSubtitle'),
-      image: 'https://images.unsplash.com/photo-1523170335258-f5ed11844a49?q=80&w=800&auto=format&fit=crop',
+      image: 'https://images.unsplash.com/photo-1601893267113-a432b85910e0?q=80&w=1074&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
       badge1: t('experiencesBadge'),
       page: 'experiences'
     }
